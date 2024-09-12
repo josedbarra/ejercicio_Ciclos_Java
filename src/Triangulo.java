530afda9d83ab89732468580d9fd1ec480835461
josedbarra <dbarraganjose@gmail.com>
2024-09-12 09:26:08 -0600

    Se realizaron los ejercicios de ciclos anidados

---
+++ src/Triangulo.java
@@ -0,0 +1,11 @@
+
+public class Triangulo {
+	public static void main(String[]args) {
+		for(int i= 1; i<7; i++) {
+			for(int j= 1; i<=i; j++) {
+				System.out.print("*");
+			}//for j
+		System.out.println();
+		}//for i
+	}//main
+}//class	
---
+++ src/TrianguloInvertido.java
@@ -0,0 +1,15 @@
+
+public class TrianguloInvertido {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i= 6; i>0; i--) {
+			for(int j= 1; i<=i; j++) {
+				System.out.print("*");
+			}//for j
+		System.out.println();
+		}//for i
+	}
+
+
+}
